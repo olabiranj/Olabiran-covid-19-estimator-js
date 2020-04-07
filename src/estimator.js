b65c91f479f933c55017d2b3ecc0accbe5822ce4
Olabiran Joshua <olabiranj@gmail.com>
2020-04-07 20:37:16 +0100

    challenges 1-3 done

--- src/estimator.js
+++ src/estimator.js
@@ -17,8 +17,10 @@ const covid19ImpactEstimator = (data) => {
   // LOGICS
   output.impact.currentlyInfected = data.reportedCases * 10;
   output.severeImpact.currentlyInfected = data.reportedCases * 50;
-  output.impact.infectionsByRequestedTime = output.impact.currentlyInfected * 512;
-  output.severeImpact.infectionsByRequestedTime = output.severeImpact.currentlyInfected * 512;
+  output.impact.infectionsByRequestedTime =
+    output.impact.currentlyInfected * 512;
+  output.severeImpact.infectionsByRequestedTime =
+    output.severeImpact.currentlyInfected * 512;
   output.impact.severeCasesByRequestedTime = Math.floor(
     output.impact.infectionsByRequestedTime * 0.15
   );
@@ -46,10 +48,16 @@ const covid19ImpactEstimator = (data) => {
     output.severeImpact.severeCasesByRequestedTime * 0.02
   );
   output.impact.dollarsInFlight = Math.floor(
-    output.impact.severeCasesByRequestedTime * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD * data.timeToElapse
+    output.impact.severeCasesByRequestedTime 
+      * data.region.avgDailyIncomePopulation
+      * data.region.avgDailyIncomeInUSD
+      * data.timeToElapse
   );
   output.severeImpact.dollarsInFlight = Math.floor(
-    output.severeImpact.severeCasesByRequestedTime * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD * data.timeToElapse
+    output.severeImpact.severeCasesByRequestedTime
+      * data.region.avgDailyIncomePopulation
+      * data.region.avgDailyIncomeInUSD
+      * data.timeToElapse
   );
 
   // OUTPUT OBJECT
