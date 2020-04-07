619403f35cb299ab06354ebfe9288e191de429cb
Olabiran Joshua <olabiranj@gmail.com>
2020-04-07 19:35:46 +0100

    challenges 1-3 done

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,63 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const output = {
+    data: { ...data }, // the input data you got
+    impact: {}, // your best case estimation
+    severeImpact: {} // your severe case estimation
+  };
+
+  const getHospitalBedsByRequestedTime = (
+    totalHospitalBeds,
+    severeCasesByRequestedTime
+  ) => {
+    let useableBedSpace = Math.floor(totalHospitalBeds * 0.35);
+    return useableBedSpace - severeCasesByRequestedTime;
+  };
+
+  output.impact.currentlyInfected = data.reportedCases * 10;
+  output.severeImpact.currentlyInfected = data.reportedCases * 50;
+  output.impact.infectionsByRequestedTime =
+    output.impact.currentlyInfected * 512;
+  output.severeImpact.infectionsByRequestedTime =
+    output.severeImpact.currentlyInfected * 512;
+  output.impact.severeCasesByRequestedTime = Math.floor(
+    output.impact.infectionsByRequestedTime * 0.15
+  );
+  output.severeImpact.severeCasesByRequestedTime = Math.floor(
+    output.severeImpact.infectionsByRequestedTime * 0.15
+  );
+  output.impact.HospitalBedsByRequestedTime = getHospitalBedsByRequestedTime(
+    data.totalHospitalBeds,
+    output.impact.severeCasesByRequestedTime
+  );
+  output.severeImpact.HospitalBedsByRequestedTime = getHospitalBedsByRequestedTime(
+    data.totalHospitalBeds,
+    output.severeImpact.severeCasesByRequestedTime
+  );
+  output.impact.casesForICUByRequestedTime = Math.floor(
+    output.impact.severeCasesByRequestedTime * 0.05
+  );
+  output.severeImpact.casesForICUByRequestedTime = Math.floor(
+    output.severeImpact.severeCasesByRequestedTime * 0.05
+  );
+  output.impact.casesForVentilatorsByRequestedTime = Math.floor(
+    output.impact.severeCasesByRequestedTime * 0.02
+  );
+  output.severeImpact.casesForVentilatorsByRequestedTime = Math.floor(
+    output.severeImpact.severeCasesByRequestedTime * 0.02
+  );
+  output.impact.dollarsInFlight = Math.floor(
+    output.impact.severeCasesByRequestedTime *
+      data.region.avgDailyIncomePopulation *
+      data.region.avgDailyIncomeInUSD *
+      data.timeToElapse
+  );
+  output.severeImpact.dollarsInFlight = Math.floor(
+    output.severeImpact.severeCasesByRequestedTime *
+      data.region.avgDailyIncomePopulation *
+      data.region.avgDailyIncomeInUSD *
+      data.timeToElapse
+  );
+  return output;
+};
 
 export default covid19ImpactEstimator;
