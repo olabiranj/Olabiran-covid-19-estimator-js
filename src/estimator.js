4b871328fb36bb4cea77bd77bd3d10b4c519f4ec
Olabiran Joshua <olabiranj@gmail.com>
2020-04-08 19:00:54 +0100

    esLint error fixed

--- src/estimator.js
+++ src/estimator.js
@@ -1,65 +1,55 @@
-const covid19ImpactEstimator = (data) => {
-  const output = {
+const covid19ImEstimator = (data) => {
+  const OP = {
     data: { ...data }, // the input data you got
-    impact: {}, // your best case estimation
-    severeImpact: {} // your severe case estimation
+    Im: {}, // your best case estimation
+    SI: {} // your severe case estimation
   };
 
-  const getHospitalBedsByRequestedTime = (
-    totalHospitalBeds,
-    severeCasesByRequestedTime
-  ) => {
+  const getHBRT = (totalHospitalBeds, SCRT) => {
     const useableBedSpace = Math.floor(totalHospitalBeds * 0.35);
-    return useableBedSpace - severeCasesByRequestedTime;
+    return useableBedSpace - SCRT;
   };
-
-  output.impact.currentlyInfected = data.reportedCases * 10;
-  output.severeImpact.currentlyInfected = data.reportedCases * 50;
-  output.impact.infectionsByRequestedTime =
-    output.impact.currentlyInfected * 512;
-  output.severeImpact.infectionsByRequestedTime =
-    output.severeImpact.currentlyInfected * 512;
-  output.impact.severeCasesByRequestedTime = Math.floor(
-    output.impact.infectionsByRequestedTime * 0.15
+  const ADIP = data.region.avgDailyIncomePopulation;
+  const ADIU = data.region.avgDailyIncomeInUSD;
+  OP.Im.CI = data.reportedCases * 10;
+  OP.SI.CI = data.reportedCases * 50;
+  OP.Im.IRT = OP.Im.CI * 512;
+  OP.SI.IRT = OP.SI.CI * 512;
+  OP.Im.SCRT = Math.floor(OP.Im.IRT * 0.15);
+  OP.SI.SCRT = Math.floor(OP.SI.IRT * 0.15);
+  OP.Im.HBRT = getHBRT(data.totalHospitalBeds, OP.Im.SCRT);
+  OP.SI.HBRT = getHBRT(data.totalHospitalBeds, OP.SI.SCRT);
+  OP.Im.CFIRT = Math.floor(OP.Im.SCRT * 0.05);
+  OP.SI.CFIRT = Math.floor(OP.SI.SCRT * 0.05);
+  OP.Im.CFVRT = Math.floor(OP.Im.SCRT * 0.02);
+  OP.SI.CFVRT = Math.floor(OP.SI.SCRT * 0.02);
+  OP.Im.dollarsInFlight = Math.floor(
+    OP.Im.SCRT * ADIP * ADIU * data.timeToElapse
   );
-  output.severeImpact.severeCasesByRequestedTime = Math.floor(
-    output.severeImpact.infectionsByRequestedTime * 0.15
-  );
-  output.impact.HospitalBedsByRequestedTime = getHospitalBedsByRequestedTime(
-    data.totalHospitalBeds,
-    output.impact.severeCasesByRequestedTime
-  );
-  output.severeImpact.HospitalBedsByRequestedTime = getHospitalBedsByRequestedTime(
-    data.totalHospitalBeds,
-    output.severeImpact.severeCasesByRequestedTime
-  );
-  output.impact.casesForICUByRequestedTime = Math.floor(
-    output.impact.severeCasesByRequestedTime * 0.05
-  );
-  output.severeImpact.casesForICUByRequestedTime = Math.floor(
-    output.severeImpact.severeCasesByRequestedTime * 0.05
-  );
-  output.impact.casesForVentilatorsByRequestedTime = Math.floor(
-    output.impact.severeCasesByRequestedTime * 0.02
-  );
-  output.severeImpact.casesForVentilatorsByRequestedTime = Math.floor(
-    output.severeImpact.severeCasesByRequestedTime * 0.02
-  );
-  output.impact.dollarsInFlight = Math.floor(
-    output.impact.severeCasesByRequestedTime *
-      data.region.avgDailyIncomePopulation *
-      data.region.avgDailyIncomeInUSD *
-      data.timeToElapse
-  );
-  output.severeImpact.dollarsInFlight = Math.floor(
-    output.severeImpact.severeCasesByRequestedTime *
-      data.region.avgDailyIncomePopulation *
-      data.region.avgDailyIncomeInUSD *
-      data.timeToElapse
+  OP.SI.dollarsInFlight = Math.floor(
+    OP.SI.SCRT * ADIP * ADIU * data.timeToElapse
   );
 
-  // output object
-  return { ...output };
+  // OP object
+  return {
+    data: { ...data },
+    impact: {
+      currentlyInfected: OP.Im.CI,
+      infectionsByRequestedTime: OP.Im.IRT,
+      severeCasesByRequestedTime: OP.Im.SCRT,
+      hospitalBedsByRequestedTime: OP.Im.HBRT,
+      casesForICUByRequestedTime: OP.Im.CFIRT,
+      casesForVentilatorsByRequestedTime: OP.Im.CFVRT
+    },
+    severeImpact: {
+      currentlyInfected: OP.SI.CI,
+      infectionsByRequestedTime: OP.SI.IRT,
+      severeCasesByRequestedTime: OP.SI.SCRT,
+      hospitalBedsByRequestedTime: OP.SI.HBRT,
+      casesForICUByRequestedTime: OP.SI.CFIRT,
+      casesForVentilatorsByRequestedTime: OP.SI.CFVRT
+    }
+  };
 };
 
-export default covid19ImpactEstimator;
+export default covid19ImEstimator;
